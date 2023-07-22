a408c7010948b140b92f79b5abcfcf7bea99689a
Taran-Grewal <tarangrewal005@gmail.com>
2023-07-22 08:23:35 +0530

    make a function playerSelection which will get prompt from the player. make a new function name playground,whichwill have two parameters playerSelection and computerselection. we'll put our getComputerChoice function in variable computerselection. we,ll use is if else statements for the game to work.

--- script.js
+++ script.js
@@ -2,11 +2,42 @@ const rock = "rock";
 const paper= "paper";
 const scissors = "scissors";
 
+function playerSelection(){
+  prompt("choose rock paper or scissor")
+
+
+
+}
 function getComputerChoice(values){
   
-        return values[Math.floor(Math.random()*values.length)];
-  
-  
+  return values[Math.floor(Math.random()*values.length)];
+
+
 }
+
+
+
+function playGround(playerSelection,computerSelection){
+  if(playerSelection == computerSelection){
+  return "draw";
+  }
+  else if(playerSelection==rock &&computerSelection==paper){
+    return "ou loose";
+  }
+  else if(playerSelection==paper &&computerSelection==scissors){
+    return "ou loose";
+}
+else if(playerSelection==scissors &&computerSelection==rock){
+  return "ou loose";
+}
+else{
+  return "you win";
+}
+}
+
 let values = [rock,paper,scissors];
+const computerSelection =getComputerChoice(values);
+
 console.log(getComputerChoice(values));
+console.log(playerSelection());
+console.log(playGround(playerSelection,computerSelection));
